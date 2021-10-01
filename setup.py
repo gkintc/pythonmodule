df7a168674208612384cd71a281b690a1cd75414
gkintc <gowtham4intc@gmail.com>
2021-09-30 17:16:52 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+print("imported my module")
+
+test = 'gowtham'
+
+def find_index(to_search, target):
+    for i, value in enumerate(to_search):
+        if value == target:
+            return i
+
+    return -1
+
